ce8712459658c427cbe071881f5d499d9beb0ffb
Quan <dinhquan191@gmail.com>
2022-07-25 11:42:31 +0700

    Initial Commit

---
+++ UIKitMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitMVVM
+//
+//  Created by Quan on 25/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
